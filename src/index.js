cfa7c372115457f1cb323527e84c15ee9c0e6626
Bossolin <vladyslav.okipnyak@icloud.com>
2021-12-01 22:09:25 +0100

    changed some css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import "./style.css";
 import UI from "./components/UI";
-import Todo from "./components/Todo";
 
-(() => {
-  window.onload = UI.generateUI();
-})();
+window.onload = UI.generateUI();
